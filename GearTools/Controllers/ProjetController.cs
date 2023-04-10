04385b9e719f116591fa522baaa8dd1dacafedcc
ahmedoumezzine <ahmedoumezzine>
2023-04-10 02:43:59 -0400

    add first exemple of signalR

--- GearTools/Controllers/ProjetController.cs
+++ GearTools/Controllers/ProjetController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using GearTools.Util;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 
@@ -17,6 +20,23 @@ namespace GearTools.Controllers
         public ActionResult CreateLayoutDashboard()
         {
             return View();
+        }
+        public JsonResult LongRunningProcess()
+        {
+            //THIS COULD BE SOME LIST OF DATA
+            int itemsCount = 100;
+
+            for (int i = 0; i <= itemsCount; i++)
+            {
+                //SIMULATING SOME TASK
+                Thread.Sleep(500);
+
+                //CALLING A FUNCTION THAT CALCULATES PERCENTAGE AND SENDS THE DATA TO THE CLIENT
+                Functions.SendProgress("Process in progress..." +DateTime.Now.Second, i, itemsCount);
+            }
+
+            return Json("", JsonRequestBehavior.AllowGet);
+
         }
         [HttpPost]
         public ActionResult CreateLayoutDashboard(string baseUrl, string url,string wrapperClass)
